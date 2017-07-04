68a9e2c5639829267249ac08ffa6d2a9f6517117
zhangxin <zhangxin2540@sina.com>
2017-07-04 12:35:57 +0800

    4

--- tasks/pages.js
+++ tasks/pages.js
@@ -7,4 +7,5 @@ import args from './util/args';//命令行参数进行解析
 gulp.task('pages',()=>{
     return gulp.src('app/**/*.ejs')//打开app下所有的ejs文件
     .pipe(gulp.dest('server'))//把所有ejs原封不动的复制到server目录下
+    .pipe(gulpif(args.watch,livereload()))
 })
